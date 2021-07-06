5b15425ee3f951fdbdac816d9e755e0a33c70e29
johnsteve108 <buddhychoi@gmail.com>
2021-07-06 20:12:17 +0900

    update

--- src/screens/post/index.js
+++ src/screens/post/index.js
@@ -5,12 +5,12 @@ import JupyterViewer from "react-jupyter-notebook";
 
 const gfm = require('remark-gfm');
 
-export const Post = ({content = {date: '2021.07.04', title: 'Chap 2-4: Perceptron to Training Neural Network', markdown: `Just a link: https://reactjs.com.`}}) => {
+export const Post = ({content = {jupyter: true, date: '2021.07.04', title: 'Chap 2-4: Perceptron to Training Neural Network', markdown: `Just a link: https://reactjs.com.`}}) => {
     const [error, setError] = useState(null);
     const [isLoaded, setIsLoaded] = useState(false);
     const [data, setData] = useState([]);
     useEffect(() => {
-      fetch('https://raw.githubusercontent.com/steveive8/Study-Deep-Learning-from-scratch/main/Chap%202-4%3A%20Perceptron_to_Training_Neural_Network.ipynb')
+      content.jupyter && fetch('https://raw.githubusercontent.com/steveive8/Study-Deep-Learning-from-scratch/main/Chap%202-4%3A%20Perceptron_to_Training_Neural_Network.ipynb')
         .then(res => res.json())
         .then(
           (result) => {
@@ -23,7 +23,7 @@ export const Post = ({content = {date: '2021.07.04', title: 'Chap 2-4: Perceptro
             setError(error);
           }
         )
-    }, [])
+    }, [content.jupyter])
     useEffect(() => {
         const styler = () => {
             const h2s = document.querySelectorAll('h2');
@@ -50,8 +50,8 @@ export const Post = ({content = {date: '2021.07.04', title: 'Chap 2-4: Perceptro
               }
             }
         }
-        styler();
-    }, [isLoaded])
+        content.jupyter && styler();
+    }, [content.jupyter, isLoaded])
     console.log(data);
     return (
         <div className="post" style={{position: 'fixed', width: '100vw', padding: '15%', paddingTop: '10%', height: '100%', overflowY: 'scroll'}}>
@@ -65,12 +65,11 @@ export const Post = ({content = {date: '2021.07.04', title: 'Chap 2-4: Perceptro
                 </Row>
                 <Text weight="700" size="30">{content.title}</Text>
               </Col>
-              <div className="jupyter" style={{marginTop: 30}}>
-                  {/* <ReactMarkdown remarkPlugins={[gfm]} children={content.markdown} /> */}
-                  <JupyterViewer codeBlockStyles={{hljsStyle: "stackoverflowDark"}} rawIpynb={data} />
-              </div>
-            </>
-           }
+              {content.jupyter ?
+                  <div className="jupyter" style={{marginTop: 30}}>
+                      <JupyterViewer codeBlockStyles={{hljsStyle: "stackoverflowDark"}} rawIpynb={data} />
+                  </div> : <ReactMarkdown remarkPlugins={[gfm]} children={content.markdown} />}
+            </>}
         </div>
     )
 }
